ff18cf1eef62e3719f44e89d5465944d0ffe1715
jiangjiawei <1781749069@qq.com>
2021-12-23 13:59:42 +0800

    2021年12月23日13:59:35提交

--- question/question011.py
+++ question/question011.py
@@ -10,4 +10,29 @@
 注意：假设数据是通过控制台输入的。
 提示：
 如果向问题提供输入数据，则应假定它是控制台输入。
+二进制转十进制：
+    例：将1110110转换为十进制数
+    解：先将其转换为字符串，再使用int函数，指定进制转换为十进制。
+    print(int("100111",2))
 """
+class Question011:
+    def __init__(self):
+        pass
+
+    def test_fun(self):
+        str_num = input("请以逗号分隔的 4 位二进制数作为输入：").split(",")
+        int_list = list()
+        for x in range(len(str_num)):
+            #print(str_num,str_num[x])
+            int_num = int(str_num[x],2)
+            print(int_num)
+            if int_num%5 == 0:
+                #print(str_num)
+                int_list.append(str_num[x])
+
+        print(int_list)
+        # for x in range(len(int_list)):
+        #     print(int_list[x],sep=",")
+
+if __name__ == "__main__":
+    Question011().test_fun()
