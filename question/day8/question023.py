f71d85e4f4a844e435416c4f2cfd1b460c37356c
jiangjiawei <1781749069@qq.com>
2021-12-31 13:34:31 +0800

    2021年12月31日13:34:26新增question023

---
+++ question/day8/question023.py
@@ -0,0 +1,19 @@
+"""
+来源：https://github.com/darkprinx/break-the-ice-with-python/blob/master/Status/Day%208.md
+问题 23
+问题：
+写一个计算数字平方值的方法
+提示：
+使用可以写成 n**p 的 ** 运算符，其中表示 n^p
+"""
+class Question023:
+    def __init__(self):
+        pass
+
+    def square(self,num):
+        # pass
+        return num**2
+
+
+if __name__ == '__main__':
+    print(Question023().square(2))
